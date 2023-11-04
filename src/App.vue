ab5621910723e4801cf47e967a9afcf7edb215a1
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2023-11-04 10:48:21 +0900

    svg

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+import Play from '@/assets/play.svg';
+import Reset from '@/assets/rotate-ccw.svg';
+import VolumeMax from '@/assets/volume-2.svg';
+import VolumeMin from '@/assets/volume-x.svg';
+</script>
+
 <template>
   <header class="p-4 text-center text-3xl font-light">
     Simple Pomodoro Timer
@@ -9,37 +16,10 @@
     </div>
     <div class="flex gap-x-4">
       <button class="btn btn-circle btn-secondary btn-outline">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="lucide lucide-rotate-ccw"
-        >
-          <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
-          <path d="M3 3v5h5" />
-        </svg>
+        <Reset />
       </button>
       <button class="btn btn-circle btn-primary">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="lucide lucide-play"
-        >
-          <polygon points="5 3 19 12 5 21 5 3" />
-        </svg>
+        <Play />
       </button>
     </div>
     <div class="grid grid-cols-[1fr_32px_1fr] items-center gap-x-4 gap-y-3">
@@ -71,22 +51,7 @@
       </fieldset>
     </div>
     <div class="flex w-full gap-x-2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="1"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="lucide lucide-volume-x"
-      >
-        <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5" />
-        <line x1="22" x2="16" y1="9" y2="15" />
-        <line x1="16" x2="22" y1="9" y2="15" />
-      </svg>
+      <VolumeMin />
       <input
         type="range"
         class="range range-secondary range-sm grow"
@@ -95,22 +60,7 @@
         step="0.1"
         :value="0.5"
       />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="1"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="lucide lucide-volume-2"
-      >
-        <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5" />
-        <path d="M15.54 8.46a5 5 0 0 1 0 7.07" />
-        <path d="M19.07 4.93a10 10 0 0 1 0 14.14" />
-      </svg>
+      <VolumeMax />
     </div>
   </main>
 
